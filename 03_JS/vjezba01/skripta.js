b477ceb4491931a21a0cd2a0978667b1de6d4b3e
Mameyeva-create <mameyeva@gmail.com>
2026-07-13 21:06:35 +0200

    Uvod u Javascript, hello world

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,22 @@
+// Ovo je komentar
+//alert('Hello world 1') // ; se može i ne mora staviti
+
+/*
+
+Ovo je kometar kroz više linija
+
+*/
+
+document.getElementById('ispis').innerText='Hello world 6'
+document.getElementById('ispis').style.color='red'
+
+document.getElementById('ge').innerHTML ='<a href="#"> Ovo je povjeznica </a>'
+
+
+//Backtics
+console.log(` 
+
+Ovaj tekst mogu pisati više 
+
+
+`)
