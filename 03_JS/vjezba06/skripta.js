f8c62c037b0a5b303c9d21a5db1855f86f596300
Mameyeva-create <mameyeva@gmail.com>
2026-07-21 19:45:51 +0200

    switch uvjetno grananje / switch zadaci

---
+++ 03_JS/vjezba06/skripta.js
@@ -0,0 +1,41 @@
+// switch
+const ocjena = 5
+
+switch (ocjena) {
+    case 1:
+        console.log('Nedovoljan')
+        break
+    case 2:
+        console.log('Dovoljan')
+        break
+    case 3:
+        console.log('Dobar')
+        break
+    case 4:
+        console.log('Vrlo dobar')
+        break
+    case 5:
+        console.log('Izvrstan')
+        break
+    default:
+        console.log('Nije ocjena')
+}
+
+
+const dan = 'Utorak'
+
+switch (dan) {
+    case 'Ponedjeljak':
+    case 'Utorak':
+    case 'Srijeda':
+    case 'Čenvrtak':
+    case 'Petak':
+        console.log('Radni dan')
+        break
+    case 'Subota':
+    case 'Nedjelja':
+        console.log('Vikend')
+        break
+    default:
+        console.log('Neispravan dan')
+}
