244b988fc1093594de8412ddfa2c4b03199a2e10
Mameyeva-create <mameyeva@gmail.com>
2026-09-10 15:36:12 +0200

    dz_s_JS_vjezba12

--- 03_JS/vjezba12/app.js
+++ 03_JS/vjezba12/app.js
@@ -3,9 +3,10 @@
 const zadaci = {
   z1: (a, b) => {
     console.log('Zadatak 1');
+    /* 1. Ispiši apsolutnu vrijednost razlike brojeva A i B. */
     // početak 1
- rezultat.innerHTML = ''
-    if(!a.trim()){
+ rezultat.innerHTML = ''// Проверяем внесены ли данные в А и В
+    if(!a.trim()){ // trim -> убирает пробелы слева и справа
       rezultat.innerHTML = 'prazno polje a'
       return
     }
@@ -13,15 +14,12 @@ const zadaci = {
       rezultat.innerHTML = 'prazno polje b'
       return
     }
-    // console.log(a,b)
     const broja = Number(a)
     const brojb = Number(b)
-    
     if(!broja){
       rezultat.innerHTML= 'a nije broj'
       return
     }
-
     if(!brojb){
       rezultat.innerHTML= 'b nije broj'
       return
@@ -29,17 +27,18 @@ const zadaci = {
 //   const razlika = broja- brojb
 //   if(razlika<0){
 //     rezultat.innerHTML =  razlika * -1
-//   }else{ 
+//   } else { 
 //     rezultat.innerHTML = razlika
 //   }
 // console.log(razlika)
 rezultat.innerHTML = Math.abs(broja -brojb)
-
     // kraj 1
   },
   z2: (a, b) => {
     console.log('Zadatak 2');
+   /* 2. Ispiši veći broj od dva unesena cijela broja A i B. */
     // početak 2
+    // Проверяем внесены ли данные:
 rezultat.innerHTML = ''
 if(!a.trim()){
 rezultat.innerHTML = 'prazno polje a'
@@ -49,10 +48,16 @@ if(!b.trim()){
   rezultat.innerHTML = 'prazno polje b'
   return
 }
+/* Аналогичное решение внесены ли данные:
+if (a.trim() =='' || b.trim() == '') {
+rezultat.innerHTML = 'Unesi brojeve'
+return
+} */
 
+// Проверяем А и В -> Числа?:
 const broja = Number(a)
 const brojb = Number(b)
-console.log(broja, brojb)
+// console.log(broja, brojb)
 if(!broja){
   rezultat.innerHTML = 'nisi unio broj a'
   return
@@ -61,20 +66,19 @@ if(!brojb){
   rezultat.innerHTML = 'nisi unio broj b'
   return
 }
-if(broja === brojb){
+if(broja === brojb) {
   rezultat.innerHTML = 'Broj a i broj b su jednaki'
-}
-  if(broja>brojb){
+} else if(broja > brojb){
     rezultat.innerHTML = broja
-  }else{
+  } else {
     rezultat.innerHTML = brojb
   }
     // kraj 2
   },
   z3: (a, b) => {
     console.log('Zadatak 3');
+    /* 3. U rezultatu napravi onoliko div elemenata koliko je uneseno u polje A te svaki drugi div ima boju pozadine definiranu u polju B. */
     // početak 3 
-
 if(a.trim()=== '' || b.trim()=== ''){
   rezultat.innerHTML = 'Unesite vrijednosti'
   return
@@ -84,15 +88,17 @@ if(!broja){
   rezultat.innerHTML = 'a nije broj'
   return
 }
+console.log(broja, b)
+
 if(broja < 1 || broja >100){
   rezultat.innerHTML ='Broj ne smije biti negativan i veci od 100'
   return
 }
 let divovi = ''
-for(let i=0; i<broja; i++){
-  if(i % 2 === 0){
+for(let i=0; i < broja; i++){
+  if(i % 2 === 0){ // Проверяем парные ли Числа
     divovi +=` 
-    <div style="background-color :${b}">&nbsp;</div>
+    <div style="background-color: ${b}">&nbsp;</div>
     `
   }else{
     divovi+='<div>&nbsp;</div>'
@@ -100,20 +106,19 @@ for(let i=0; i<broja; i++){
 }
 rezultat.innerHTML = divovi
 
-
 console.log(broja, b)
-
-
     // kraj 3
   },
   z4: (a, b) => {
     console.log('Zadatak 4');
+    /* 4. Ako ostane polje A i B prazno u rezultat upišite dva emojia po vlastitom izboru */
     // početak 4
 
     // kraj 4
   },
   z5: (a, b) => {
     console.log('Zadatak 5');
+    /* 5. Osmislite zadatak sami i realizirajte ga */
     // početak 5
 
     // kraj 5
